b178334db6012b4880876ff4ba2600dbe51ac295
Sravan Sai Kumar Kotha <sravan.kotha@office365.3pillarglobal.com>
2024-06-29 14:04:38 +0530

    updating yml file

--- cypress/e2e/step_definations/pages/pocLogin/pocLogin.ts
+++ cypress/e2e/step_definations/pages/pocLogin/pocLogin.ts
@@ -21,14 +21,33 @@ export class PocLogin {
               });
       
               cy.visit(siteURL);
-              cy.contains('Login').click();
-              cy.frameLoaded('div[id="root"]>div>iframe')
-              cy.iframe('div[id="root"]>div>iframe').xpath('//*[@id="loginBtn"]').click();
+              cy.contains('Register Now').click();
+              cy.wait(1000)
+              // cy.frameLoaded('div[id="root"]>div>div>div>div>iframe')
+              // cy.iframe('div[id="root"]>div>div>div>div>iframe').find('button[id="loginBtn"]').click();
       
-              cy.iframe('div[id="root"]>div>iframe').find('input[name="email"]').type(UserName);      
-              cy.iframe('div[id="root"]>div>iframe').find('input[name="password"]').type("Buyer@123");
+              // cy.iframe('div[id="root"]>div>iframe').find('input[name="email"]').type(UserName);      
+              // cy.iframe('div[id="root"]>div>iframe').find('input[name="password"]').type("Buyer@123");
       
-              cy.iframe('div[id="root"]>div>iframe').find('button[class="iiris-aThJd iiris-btn iiris-btn-block"]').eq(0).click();
+              // cy.iframe('div[id="root"]>div>iframe').find('button[class="iiris-aThJd iiris-btn iiris-btn-block"]').eq(0).click();
+              cy.get('[title="External HTML"]')
+                .its("0.contentDocument.body")
+                .find("#loginBtn").click()
+              cy.get('[title="External HTML"]')
+                .its("0.contentDocument.body")
+                .find('input[name="email"]').type(UserName)
+              cy.get('[title="External HTML"]')
+                .its("0.contentDocument.body")  
+                .find('input[name="password"]').type("Buyer@123")
+              cy.get('[title="External HTML"]')
+                .its("0.contentDocument.body")
+                .find('button[class="iiris-aThJd iiris-btn iiris-btn-block"]').eq(0).click();
+              cy.wait(10000)
+
+              cy.get('[title="External HTML"]')
+                .its("0.contentDocument.body")
+                .find('button[class="iiris-bfnSK iiris-btn iiris-btn-block "]').click();
+
               cy.wait(10000)
               //cy.reload();
       
